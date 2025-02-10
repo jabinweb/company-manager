a76b4775d02da5ce498981bc2f4564723b125905
Harshit Singh <jabincreators@gmail.com>
2025-02-10 14:30:35 +0530

    Updated Next Config

--- next.config.js
+++ next.config.js
@@ -8,22 +8,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
-    ]
-  },
-  // experimental: {
-  //   serverActions: true,
-  // }
 }
 
 module.exports = nextConfig
